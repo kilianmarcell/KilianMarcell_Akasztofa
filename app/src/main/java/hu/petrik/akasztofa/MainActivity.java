ee44e4b6b75ee49d75bbf5b342a2afdb9e5d51df
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-24 19:12:33 +0200

    Inicializálás kész

--- app/src/main/java/hu/petrik/akasztofa/MainActivity.java
+++ app/src/main/java/hu/petrik/akasztofa/MainActivity.java
@@ -3,12 +3,30 @@ package hu.petrik.akasztofa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button gombMinusz, gombPlusz, gombTippel;
+    private TextView tippelBetu, kitalalSzo;
+    private ImageView kepAkasztofa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init() {
+        gombMinusz = findViewById(R.id.gombMinusz);
+        gombPlusz = findViewById(R.id.gombPlusz);
+        gombTippel = findViewById(R.id.gombTippel);
+        tippelBetu = findViewById(R.id.tippelBetu);
+        kitalalSzo = findViewById(R.id.kitalalSzo);
+        kepAkasztofa = findViewById(R.id.kepAkasztofa);
     }
 }

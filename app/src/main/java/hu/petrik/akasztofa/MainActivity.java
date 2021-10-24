9f13b558d66500f0119c0a88e29d16f12f1c0322
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-25 01:10:30 +0200

    Kód rendezése

--- app/src/main/java/hu/petrik/akasztofa/MainActivity.java
+++ app/src/main/java/hu/petrik/akasztofa/MainActivity.java
@@ -6,11 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -37,8 +35,10 @@ public class MainActivity extends AppCompatActivity {
 
     private int tippekSzamlalo = 0;
     private int hanyadikBetu = 0;
+
     private char melyikBetu = betuTomb[hanyadikBetu];
     private String kivalasztottSzo = szoTomb[(int)(Math.random() * szoTomb.length)];
+
     private String beallitasKitalalSzo = "";
 
     @Override
@@ -51,11 +51,15 @@ public class MainActivity extends AppCompatActivity {
 
         gombPlusz.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
+
                 hanyadikBetu++;
+
                 if (hanyadikBetu > betuTomb.length-1) {
                     hanyadikBetu = 0;
                 }
+
                 vizsgalSzin();
                 tippelBetuBeallitas();
             }
@@ -63,11 +67,15 @@ public class MainActivity extends AppCompatActivity {
 
         gombMinusz.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
+
                 hanyadikBetu--;
+
                 if (hanyadikBetu < 0) {
                     hanyadikBetu = betuTomb.length-1;
                 }
+
                 vizsgalSzin();
                 tippelBetuBeallitas();
             }
@@ -81,50 +89,78 @@ public class MainActivity extends AppCompatActivity {
                 vizsgalBetu();
             }
         });
+
     }
 
     public void resetMethod() {
+
+        tippekSzamlalo = 0;
+        hanyadikBetu = 0;
+
         kivalasztottSzo = szoTomb[(int)(Math.random() * szoTomb.length)];
+
+        kepAkasztofa.setImageResource(R.drawable.akasztofa00);
+
         tippeltBetuk.clear();
+
         beallitasKitalalSzo = "";
+
         for (int i = 0; i < kivalasztottSzo.length(); i++) {
             beallitasKitalalSzo += "_ ";
         }
+
         kitalalSzo.setText(beallitasKitalalSzo);
         tippelBetu.setText(melyikBetu + "");
+
         tippelBetu.setTextColor(Color.RED);
+
     }
 
     public void vizsgalSzin() {
+
         if (!(tippeltBetuk.contains(betuTomb[hanyadikBetu]))) {
+
             tippelBetu.setTextColor(Color.RED);
+
         } else {
+
             tippelBetu.setTextColor(Color.BLACK);
         }
+
     }
 
     public void vizsgalBetu() {
+
         StringBuilder kitalalandoSzo = new StringBuilder(beallitasKitalalSzo);
 
         boolean vanE = false;
 
         for (int i = 0; i < kivalasztottSzo.length(); i++) {
+
             if (Character.toUpperCase(kivalasztottSzo.charAt(i)) == betuTomb[hanyadikBetu]) {
+
                 kitalalandoSzo.setCharAt(i * 2, betuTomb[hanyadikBetu]);
                 vanE = true;
+
             }
         }
+
         if (!vanE) {
+
             tippekSzamlalo++;
 
             kepValtas(tippekSzamlalo);
+
         }
+
         beallitasKitalalSzo = kitalalandoSzo.toString();
 
         kitalalSzo.setText(beallitasKitalalSzo);
 
         if (!(beallitasKitalalSzo.contains("_"))) {
+
             jatekVege(true);
+
         }
 
         //for (int i = 0; i < kivalasztottSzo.length(); i++) {
@@ -137,6 +173,7 @@ public class MainActivity extends AppCompatActivity {
         //        beallitasKitalalSzo = elotte + Character.toLowerCase(betuTomb[hanyadikBetu]) + utana;
         //    }
         //}
+
     }
 
     public void kepValtas(int hanyadikHiba) {
@@ -183,50 +220,59 @@ public class MainActivity extends AppCompatActivity {
                 jatekVege(false);
                 break;
         }
+
     }
 
     public void jatekVege(boolean nyertE) {
+
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
         if (nyertE) {
+
             alert.setTitle("Helyes megfejtés!");
+
         } else {
+
             alert.setTitle("Nem sikerült kitalálni!");
+
         }
             alert.setMessage("Szeretnél még egyet játszani?");
+
         alert.setCancelable(false);
+
         alert.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 finish();
             }
         });
+
         alert.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                ujJatek();
+                resetMethod();
             }
         });
         alert.create().show();
-    }
 
-    public void ujJatek() {
-        kepAkasztofa.setImageResource(R.drawable.akasztofa00);
-        tippekSzamlalo = 0;
-        hanyadikBetu = 0;
-        resetMethod();
     }
 
     public void tippelBetuBeallitas() {
+
         char melyikBetu = betuTomb[hanyadikBetu];
+
         tippelBetu.setText(melyikBetu + "");
+
     }
 
     public void init() {
+
         gombMinusz = findViewById(R.id.gombMinusz);
         gombPlusz = findViewById(R.id.gombPlusz);
         gombTippel = findViewById(R.id.gombTippel);
         tippelBetu = findViewById(R.id.tippelBetu);
         kitalalSzo = findViewById(R.id.kitalalSzo);
         kepAkasztofa = findViewById(R.id.kepAkasztofa);
+
     }
 }

05d41f8036971d72d94609d4173fa75d1ad279fd
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-24 19:49:20 +0200

    Plusz és mínusz gombokra kattintva abc betűi változnak

--- app/src/main/java/hu/petrik/akasztofa/MainActivity.java
+++ app/src/main/java/hu/petrik/akasztofa/MainActivity.java
@@ -3,6 +3,7 @@ package hu.petrik.akasztofa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,15 +13,20 @@ public class MainActivity extends AppCompatActivity {
     private Button gombMinusz, gombPlusz, gombTippel;
     private TextView tippelBetu, kitalalSzo;
     private ImageView kepAkasztofa;
+
     private String[] szoTomb = {
             "implementálás", "objektum", "rekurzió", "testnevelés", "számítástechnika",
             "pendrive", "mérnökinformatikus", "feladatok", "depresszió", "túlóra"
     };
+
     private String[] betuTomb = {
             "A", "Á", "B", "C", "D", "E", "É", "F", "G", "H", "I", "Í", "J",
             "K", "L", "M", "N", "O", "Ó", "Ö", "Ő", "P", "Q", "R", "S", "T",
             "U", "Ú", "Ü", "Ű", "V", "W", "X", "Y", "Z"
     };
+
+    private int hanyadikBetu = 0;
+    private String melyikBetu = betuTomb[hanyadikBetu];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +42,32 @@ public class MainActivity extends AppCompatActivity {
             beallitasKitalalSzo += "_ ";
         }
         kitalalSzo.setText(beallitasKitalalSzo);
-        int melyikSzam = 0;
+
+        tippelBetu.setText(melyikBetu);
+
+        gombPlusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hanyadikBetu++;
+                if (hanyadikBetu > betuTomb.length-1) {
+                    hanyadikBetu = 0;
+                }
+                String melyikBetu = betuTomb[hanyadikBetu];
+                tippelBetu.setText(melyikBetu);
+            }
+        });
+
+        gombMinusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hanyadikBetu--;
+                if (hanyadikBetu < 0) {
+                    hanyadikBetu = betuTomb.length-1;
+                }
+                String melyikBetu = betuTomb[hanyadikBetu];
+                tippelBetu.setText(melyikBetu);
+            }
+        });
     }
 
     public void init() {

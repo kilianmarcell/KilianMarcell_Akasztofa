1500efbc382c729dcd128277159e5cc93e8faf11
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-10-24 19:32:50 +0200

    Szavak és betűk hozzáadva

--- app/src/main/java/hu/petrik/akasztofa/MainActivity.java
+++ app/src/main/java/hu/petrik/akasztofa/MainActivity.java
@@ -12,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     private Button gombMinusz, gombPlusz, gombTippel;
     private TextView tippelBetu, kitalalSzo;
     private ImageView kepAkasztofa;
+    private String[] szoTomb = {
+            "implementálás", "objektum", "rekurzió", "testnevelés", "számítástechnika",
+            "pendrive", "mérnökinformatikus", "feladatok", "depresszió", "túlóra"
+    };
+    private String[] betuTomb = {
+            "a", "á", "b", "c", "d", "e", "é", "f", "g", "h", "i", "í", "j",
+            "k", "l", "m", "n", "o", "ó", "ö", "ő", "p", "q", "r", "s", "t",
+            "u", "ú", "ü", "ű", "v", "w", "x", "y", "z"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+        int melyikSzam = 0;
+
     }
 
     public void init() {
